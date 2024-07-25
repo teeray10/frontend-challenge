f6afb8870450797931f14e1889d5614232fea8bb
Taylor Ray <taylorray.tray@gmail.com>
2024-07-25 23:20:51 +0200

    Add MailingList component

---
+++ src/app/shoes/_components/sidebar/mailingList.tsx
@@ -0,0 +1,20 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faEnvelope} from '@fortawesome/free-regular-svg-icons/faEnvelope';
+import {faXmark} from '@fortawesome/free-solid-svg-icons/faXmark';
+
+export default function MailingList() {
+  return (
+    <div className="bg-blue text-white flex flex-col items-center gap-4 py-5 px-7 relative lg:ml-5">
+      <FontAwesomeIcon icon={faXmark} className="absolute right-3 top-3 w-5 h-5"/>
+      <FontAwesomeIcon icon={faEnvelope} className="w-10 h-10"/>
+      <h4 className="font-suez text-xl">SIGN UP TO OUR MAILING LIST!</h4>
+
+      <div className="w-full">
+        <p className="ml-3 mb-1 font-bold text-sm">EMAIL</p>
+        <input type="text" placeholder="ENTER EMAIL ADDRESS" className="w-full py-2 px-3" />
+      </div>
+
+      <button className="bg-white text-blue font-suez text-xl px-12 py-4">SUBMIT</button>
+    </div>
+  )
+}
--- src/app/shoes/layout.tsx
+++ src/app/shoes/layout.tsx
@@ -1,8 +1,9 @@
 import {ReactNode} from 'react';
 import SubHeader from '@/app/shoes/_components/subHeader';
-import SearchBox from '@/app/shoes/_components/searchBox';
+import SearchBox from '@/app/shoes/_components/sidebar/searchBox';
 import TopBanner from '@/app/shoes/_components/banners/topBanner';
-import SideBanner from '@/app/shoes/_components/banners/sideBanner';
+import SideBanner from '@/app/shoes/_components/sidebar/sideBanner';
+import MailingList from '@/app/shoes/_components/sidebar/mailingList';
 
 export default function ShoesLayout({children}: Readonly<{children: ReactNode}>) {
   return (
@@ -11,6 +12,7 @@ export default function ShoesLayout({children}: Readonly<{children: ReactNode}>)
       <div className="flex flex-col col-span-full lg:col-span-1">
         <SearchBox />
         <SideBanner />
+        <MailingList />
       </div>
       <div className="col-span-full lg:col-span-3 2xl:mx-20">
         <TopBanner />
