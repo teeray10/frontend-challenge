2b62537fcf515ba3f3437904c53772c449099ac6
Taylor Ray <taylorray.tray@gmail.com>
2024-07-28 15:10:18 +0200

    Fix mobile responsiveness for Header component and add routing

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,43 +1,53 @@
+"use client";
+
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faGift} from '@fortawesome/free-solid-svg-icons/faGift';
 import {faStar} from '@fortawesome/free-regular-svg-icons/faStar';
 import {faHeart} from '@fortawesome/free-regular-svg-icons/faHeart';
 import {faTag} from '@fortawesome/free-solid-svg-icons/faTag';
+import Link from 'next/link';
+import {usePathname} from 'next/navigation';
+
+const links = [
+  {name: "apparels"},
+  {name: "shoes"},
+  {name: "equipment"},
+  {name: "gift"}
+]
 
 export default function Header() {
-  return (
-    <nav className="font-suez navbar bg-base-100">
-      <div className="navbar-start">
-        <a className="btn btn-ghost text-xl">
-          <img src="/logo.svg"
-                 alt="Reway Logo"
-                 className="dark:invert"
-          />
-        </a>
-        <ul className="menu menu-horizontal px-1 hidden font-medium lg:flex">
-          <li><a>APPARELS</a></li>
-          <li><a>SHOES</a></li>
-          <li><a>EQUIPMENT</a></li>
-          <li><a>GIFT</a></li>
-        </ul>
-      </div>
-      <div className="navbar-center hidden lg:flex">
+  const path = usePathname();
+  const isActiveLink = (href: string) => path === '/' + href;
 
+  return (
+    <nav className="font-suez navbar flex justify-between p-5 lg:px-11 lg:py-6">
+      <Link href="/">
+        <img src="/logo.svg"
+             alt="Reway Logo"
+             className="dark:invert"
+        />
+      </Link>
+      <ul className="hidden font-medium lg:flex gap-8 hover:*:text-blue hover:*:cursor-pointer">
+        {
+          links?.map((link, index) => (
+            <li key={link.name}>
+              <Link href={`/${link.name}`} className={`uppercase ${isActiveLink(link.name) ? 'text-blue' : ''}`}>{link.name}</Link>
+            </li>
+          ))
+        }
+      </ul>
+      <div className="gap-5 text-grey hidden lg:flex">
+        <FontAwesomeIcon icon={faGift} className="w-6 h-5 cursor-pointer hover:text-blue" />
+        <FontAwesomeIcon icon={faStar} className="w-6 h-5 cursor-pointer hover:text-blue" />
+        <FontAwesomeIcon icon={faHeart} className="w-6 h-5 cursor-pointer hover:text-blue" />
+        <FontAwesomeIcon icon={faTag} className="w-6 h-5 cursor-pointer hover:text-blue" />
       </div>
-      <div className="navbar-end">
-        <div className="gap-5 text-grey hidden lg:flex">
-          <FontAwesomeIcon icon={faGift} className="w-6 h-5" />
-          <FontAwesomeIcon icon={faStar} className="w-6 h-5" />
-          <FontAwesomeIcon icon={faHeart} className="w-6 h-5" />
-          <FontAwesomeIcon icon={faTag} className="w-6 h-5" />
-        </div>
-        <div className="text-blue text-[17px] hidden lg:block">
-          <button>SIGN UP</button>
-          <button className="border-[3px] w-28 h-10 rounded-s">LOGIN</button>
-        </div>
+      <div className="text-blue text-[17px] hidden lg:inline">
+        <button>SIGN UP</button>
+        <button className="border-[3px] border-blue w-28 h-10 rounded-md ml-8">LOGIN</button>
       </div>
-      <div className="dropdown dropdown-end">
-        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+      <div className="dropdown dropdown-end lg:hidden">
+        <div tabIndex={0} role="button" className="btn btn-ghost">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5"
@@ -51,12 +61,25 @@ export default function Header() {
               d="M4 6h16M4 12h8m-8 6h16"/>
           </svg>
         </div>
-        <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-          <li><a>APPARELS</a></li>
-          <li><a>SHOES</a></li>
-          <li><a>EQUIPMENT</a></li>
-          <li><a>GIFT</a></li>
-        </ul>
+        <div className="dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-3 shadow flex flex-col">
+          <ul className="flex flex-col gap-1">
+            {
+              links?.map((link, index) => (
+                <li key={link.name}>
+                  <Link href={`/${link.name}`} className={`uppercase ${isActiveLink(link.name) ? 'text-blue' : ''}`}>{link.name}</Link>
+                </li>
+              ))
+            }
+          </ul>
+          <div className="flex my-3 gap-1">
+            <FontAwesomeIcon icon={faGift} className="w-6 h-5 cursor-pointer hover:text-blue"/>
+            <FontAwesomeIcon icon={faStar} className="w-6 h-5 cursor-pointer hover:text-blue"/>
+            <FontAwesomeIcon icon={faHeart} className="w-6 h-5 cursor-pointer hover:text-blue"/>
+            <FontAwesomeIcon icon={faTag} className="w-6 h-5 cursor-pointer hover:text-blue"/>
+          </div>
+          <button className="w-fit">SIGN UP</button>
+          <button className="w-fit">LOGIN</button>
+        </div>
       </div>
     </nav>
   )
